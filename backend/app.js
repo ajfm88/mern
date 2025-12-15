e7b3b71a33311a6a9fb0dc9b760a64f893924a29
ajfm88 <ale@ajfm88.com>
2025-12-15 10:21:44 -0800

    Section 07 — Node.js & Express.js - Building our REST API: 90. Module Introduction. 91. Setting up our Project. 92. Implementing Basic Routing. 93. Adding Place-Specific Routes. 94. Getting a Place By User ID. 95. Handling Errors. 96. Adding our own Error Model. 97. Adding Controllers. 98. Adding a POST Route & Using Postman. 99. Handling Errors for Unsupported Routes.

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.post('/user', (req, res, next) => {
-  res.send('<h1>User: ' + req.body.username + '</h1>');
+app.use('/api/places', placesRoutes); // => /api/places...
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
 });
 
-app.get('/', (req, res, next) => {
-  res.send(
-    '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
-  );
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
 });
 
 app.listen(5000);
